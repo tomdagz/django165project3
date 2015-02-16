3121953e093ba04492adb1ea4be9f959f572570a
Thom <tomdagz@gmail.com>
2015-02-15 22:24:33 -0600

    manipulando videos

--- apps/multi/models.py
+++ apps/multi/models.py
@@ -1,3 +1,22 @@
+# -*- encoding: utf-8 -*-
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+class Video(models.Model):
+
+  class Meta:
+
+    verbose_name = u'Video'
+    verbose_name_plural = u'Videos'
+
+  nombre = models.CharField(max_length=20)
+  cover  = models.ImageField(upload_to='galeria/video/')
+  url    = models.URLField(verbose_name=u'URL de Youtube',help_text=u'Ej. (https://www.youtube.com/watch/v=XXXXXXXXXX)',validators=[RegexValidator(regex = '^([\w\-]+)://(?:www).(?:youtube).([\w\-]+)/(?:watch).v=([\w\-]+)$', message = u'Ingrese una dirección válida de YouTube')])
+
+  def __unicode__(self):
+    return self.nombre
+
+  def get_url_id(self):
+    return self.url.split('=')[1]
