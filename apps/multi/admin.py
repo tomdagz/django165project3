177f41d60c5b00fec33c84eac192a7f6598a515d
Thom <tomdagz@gmail.com>
2015-02-21 21:49:43 -0600

    validacion antes de guardar

--- apps/multi/admin.py
+++ apps/multi/admin.py
@@ -1,6 +1,11 @@
 # -*- encoding: utf-8 -*-
 from django.contrib import admin
-from .models import Video
+from .models import Video, Ruta
+from .forms import RutaForm
 # Register your models here.
+class RutaAdmin(admin.ModelAdmin):
+    form = RutaForm
+    list_display = ('titulo', 'especial')
 
 admin.site.register(Video,)
+admin.site.register(Ruta, RutaAdmin)
